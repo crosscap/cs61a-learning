fe57a9616fe0e9eebe1cef924fcb2c6c9dbe4a84
crosscap <wangliankun2018@outlook.com>
2024-03-26 17:20:11 +0800

    finish disc05

---
+++ discussion/disc05.py
@@ -0,0 +1,102 @@
+from trees import *
+
+
+# 1.1
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    """
+    if is_leaf(t):
+        return 0
+    else:
+        return max([height(b) for b in branches(t)]) + 1
+
+
+# 1.2
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    if is_leaf(t):
+        return label(t)
+    else:
+        return label(t) + max([max_path_sum(b) for b in branches(t)])
+
+
+# 1.3
+def square_tree(t):
+    """Return a tree with the square of every element in t
+
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(square_tree(numbers))
+    1
+      4
+        9
+        16
+      25
+        36
+          49
+        64
+    """
+    if is_leaf(t):
+        return tree(label(t) ** 2)
+    else:
+        return tree(label(t) ** 2, [square_tree(b) for b in branches(t)])
+
+
+# 1.4
+def find_path(tree, x):
+    """
+    >>> t = tree(6, [tree(7, [tree(5)])])
+    >>> find_path(t, 5)
+    [6, 7, 5]
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])]), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10) # returns None
+    """
+    if label(tree) == x:
+        return [x]
+    elif not is_leaf(tree):
+        path = [find_path(b, x) for b in branches(tree) if find_path(b, x)]
+        if path != []:
+            return [label(tree)] + path[0]
+
+
+# 2.2
+# wrong
+def prune_binary(t, nums):
+    """
+    >>> t = tree('1', [tree('0', [tree('0'), tree('1')]), tree('1', [tree('0')])])
+    >>> prune binary(t, ['01', '110', '100'])
+    ['1', ['0']]
+    """
+    if is_leaf(t):
+        if label(t) in nums:
+            return t
+        return None
+    else:
+        # next_valid_nums = [num[1:] for num in nums]
+        next_valid_nums = [num[1:] for num in nums if num[0] == label(t)]
+        new_branches = []
+        for b in branches(t):
+            pruned_branch = prune_binary(b, next_valid_nums)
+            if pruned_branch is not None:
+                new_branches = new_branches + [pruned_branch]
+        if not new_branches:
+            return None
+        # return new_branches
+        return tree(label(t), new_branches)
---
+++ discussion/trees.py
@@ -0,0 +1,31 @@
+def tree(label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch)
+    return [label] + list(branches)
+
+
+def label(tree):
+    return tree[0]
+
+
+def branches(tree):
+    return tree[1:]
+
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    return not branches(tree)
+
+
+def print_tree(t, indent=0):
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent+1)
