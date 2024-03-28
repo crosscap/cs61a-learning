3b556f6739bee165f20279958b09e3a072d6f166
crosscap <wangliankun2018@outlook.com>
2024-03-28 17:51:13 +0800

    finish homework03

--- homeworks/hw03/hw03.py
+++ homeworks/hw03/hw03.py
@@ -260,12 +260,12 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[1]
 
 
 def str_interval(x):
@@ -285,24 +285,26 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    lower = lower_bound(x) - upper_bound(y)
+    upper = upper_bound(x) - lower_bound(y)
+    return interval(lower, upper)
 
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
-    "*** YOUR CODE HERE ***"
+    assert upper_bound(y) > 0 and lower_bound(y) > 0
     reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -327,13 +329,18 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 10)  # Replace this line!
+    r2 = interval(1, 10)  # Replace this line!
     return r1, r2
 
 
+# Copy from network
 def multiple_references_explanation():
-    return """The multiple reference problem..."""
+    return """She is right, because in par1, each interval is referenced twice,
+            so that the interval will be looser since the it choose different
+            values for each instance of the interval. In par2, each interval is
+            referenced only once, so the interval will be tighter.
+            """
 
 
 def quadratic(x, a, b, c):
@@ -345,10 +352,22 @@ def quadratic(x, a, b, c):
     >>> str_interval(quadratic(interval(1, 3), 2, -3, 1))
     '0 to 10'
     """
-    "*** YOUR CODE HERE ***"
-
+    assert a != 0, 'function should be a quadratic'
+    def q_fun(x): return a * x**2 + b * x + c
+    pole = -b / (2 * a)
+    lower = min(q_fun(lower_bound(x)), q_fun(upper_bound(x)))
+    upper = max(q_fun(lower_bound(x)), q_fun(upper_bound(x)))
+    print('DEBUG:', pole, q_fun(pole), lower, upper)
+    if lower_bound(x) <= pole and upper_bound(x) >= pole:
+        if a > 0:
+            return interval(q_fun(pole), upper)
+        else:
+            return interval(lower, q_fun(pole))
+    else:
+        return interval(lower, upper)
 
 # Tree ADT
+
 
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
