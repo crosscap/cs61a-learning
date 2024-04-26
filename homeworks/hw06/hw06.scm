b70cc2eed5c5f25284a94577a1d262dc8ef9422d
crosscap <wangliankun2018@outlook.com>
2024-04-26 19:58:07 +0800

    finish homework06

--- homeworks/hw06/hw06.scm
+++ homeworks/hw06/hw06.scm
@@ -2,22 +2,29 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 
 (define (sign num)
-  'YOUR-CODE-HERE
+  (cond
+    ((> num 0) 1)
+    ((= num 0) 0)
+    (else -1))
 )
 
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
+  (cond
+    ((= x 1) 1)
+    ((= y 1) x)
+    ((odd? y) (* x (pow x (- y 1))))
+    (else (square (pow x (/ y 2)))))
 )
 
