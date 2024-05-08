7a452c9674cf82b94509fe8e82b59948077ae061
crosscap <wangliankun2018@outlook.com>
2024-05-08 19:46:42 +0800

    finish l31

---
+++ ex/l31/cities.sql
@@ -0,0 +1,7 @@
+.open --new
+
+-- Cities
+create table cities as
+  select 38 as latitude, 122 as longitude, "Berkeley" as name union
+  select 42,             71,               "Cambridge"        union
+  select 45,             93,               "Minneapolis";
---
+++ ex/l31/lift.sql
@@ -0,0 +1,8 @@
+-- Arithmetic
+
+create table lift as
+  select 101 as chair, 2 as single, 2 as couple union
+  select 102         , 0          , 3           union
+  select 103         , 4          , 1;
+
+select chair, single + 2 * couple as total from lift;
