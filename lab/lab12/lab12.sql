77bd384f541ace05c604771e13bf2f8ba455e809
crosscap bell <wangliankun2018@outlook.com>
2024-05-09 22:15:24 +0800

    finish lab12

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -2,20 +2,24 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet FROM students WHERE color="blue" AND pet="dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT color, pet, song FROM students WHERE color="blue" AND pet="dog";
 
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s1.pet, s1.song, s1.color, s2.color
+  FROM students AS s1, students AS s2
+  WHERE s1.pet = s2.pet AND s1.song = s2.song AND s1.time < s2.time;
+
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT s.seven
+  FROM students AS s, numbers AS n
+  WHERE s.time = n.time AND s.number = 7 AND n.'7' = 'True';
