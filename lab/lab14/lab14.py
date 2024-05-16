6420378a83d80f2f6a469d7b681fed1995704c06
crosscap <wangliankun2018@outlook.com>
2024-05-16 14:47:12 +0800

    finish q1 to q3 of lab14

--- lab/lab14/lab14.py
+++ lab/lab14/lab14.py
@@ -17,7 +17,11 @@ def prune_min(t):
     >>> t3
     Tree(6, [Tree(3, [Tree(1)])])
     """
-    "*** YOUR CODE HERE ***"
+    if not t.is_leaf():
+        if len(t.branches) == 2:
+            max_index = 0 if t.branches[0].label > t.branches[1].label else 1
+            t.branches.pop(max_index)
+        prune_min(t.branches[0])
 
 
 def num_splits(s, d):
@@ -33,196 +37,196 @@ def num_splits(s, d):
     >>> num_splits([1, 4, 6, 8, 2, 9, 5], 3)
     12
     """
-    "*** YOUR CODE HERE ***"
 
 
-class Account(object):
-    """A bank account that allows deposits and withdrawals.
 
-    >>> eric_account = Account('Eric')
-    >>> eric_account.deposit(1000000)   # depositing my paycheck for the week
-    1000000
-    >>> eric_account.transactions
-    [('deposit', 1000000)]
-    >>> eric_account.withdraw(100)      # buying dinner
-    999900
-    >>> eric_account.transactions
-    [('deposit', 1000000), ('withdraw', 100)]
-    """
+# class Account(object):
+#     """A bank account that allows deposits and withdrawals.
 
-    interest = 0.02
+#     >>> eric_account = Account('Eric')
+#     >>> eric_account.deposit(1000000)   # depositing my paycheck for the week
+#     1000000
+#     >>> eric_account.transactions
+#     [('deposit', 1000000)]
+#     >>> eric_account.withdraw(100)      # buying dinner
+#     999900
+#     >>> eric_account.transactions
+#     [('deposit', 1000000), ('withdraw', 100)]
+#     """
 
-    def __init__(self, account_holder):
-        self.balance = 0
-        self.holder = account_holder
+#     interest = 0.02
 
-    def deposit(self, amount):
-        """Increase the account balance by amount and return the
-        new balance.
-        """
-        self.balance = self.balance + amount
-        return self.balance
+#     def __init__(self, account_holder):
+#         self.balance = 0
+#         self.holder = account_holder
 
-    def withdraw(self, amount):
-        """Decrease the account balance by amount and return the
-        new balance.
-        """
-        if amount > self.balance:
-            return 'Insufficient funds'
-        self.balance = self.balance - amount
-        return self.balance
+#     def deposit(self, amount):
+#         """Increase the account balance by amount and return the
+#         new balance.
+#         """
+#         self.balance = self.balance + amount
+#         return self.balance
 
-
-class CheckingAccount(Account):
-    """A bank account that charges for withdrawals.
-
-    >>> check = Check("Steven", 42)  # 42 dollars, payable to Steven
-    >>> steven_account = CheckingAccount("Steven")
-    >>> eric_account = CheckingAccount("Eric")
-    >>> eric_account.deposit_check(check)  # trying to steal steven's money
-    The police have been notified.
-    >>> eric_account.balance
-    0
-    >>> check.deposited
-    False
-    >>> steven_account.balance
-    0
-    >>> steven_account.deposit_check(check)
-    42
-    >>> check.deposited
-    True
-    >>> steven_account.deposit_check(check)  # can't cash check twice
-    The police have been notified.
-    """
-    withdraw_fee = 1
-    interest = 0.01
-
-    def withdraw(self, amount):
-        return Account.withdraw(self, amount + self.withdraw_fee)
-
-    "*** YOUR CODE HERE ***"
+#     def withdraw(self, amount):
+#         """Decrease the account balance by amount and return the
+#         new balance.
+#         """
+#         if amount > self.balance:
+#             return 'Insufficient funds'
+#         self.balance = self.balance - amount
+#         return self.balance
 
 
-class Check(object):
-    "*** YOUR CODE HERE ***"
+# class CheckingAccount(Account):
+#     """A bank account that charges for withdrawals.
+
+#     >>> check = Check("Steven", 42)  # 42 dollars, payable to Steven
+#     >>> steven_account = CheckingAccount("Steven")
+#     >>> eric_account = CheckingAccount("Eric")
+#     >>> eric_account.deposit_check(check)  # trying to steal steven's money
+#     The police have been notified.
+#     >>> eric_account.balance
+#     0
+#     >>> check.deposited
+#     False
+#     >>> steven_account.balance
+#     0
+#     >>> steven_account.deposit_check(check)
+#     42
+#     >>> check.deposited
+#     True
+#     >>> steven_account.deposit_check(check)  # can't cash check twice
+#     The police have been notified.
+#     """
+#     withdraw_fee = 1
+#     interest = 0.01
+
+#     def withdraw(self, amount):
+#         return Account.withdraw(self, amount + self.withdraw_fee)
+
+#     "*** YOUR CODE HERE ***"
 
 
-def align_skeleton(skeleton, code):
-    """
-    Aligns the given skeleton with the given code, minimizing the edit distance between
-    the two. Both skeleton and code are assumed to be valid one-line strings of code.
-
-    >>> align_skeleton(skeleton="", code="")
-    ''
-    >>> align_skeleton(skeleton="", code="i")
-    '+[i]'
-    >>> align_skeleton(skeleton="i", code="")
-    '-[i]'
-    >>> align_skeleton(skeleton="i", code="i")
-    'i'
-    >>> align_skeleton(skeleton="i", code="j")
-    '+[j]-[i]'
-    >>> align_skeleton(skeleton="x=5", code="x=6")
-    'x=+[6]-[5]'
-    >>> align_skeleton(skeleton="return x", code="return x+1")
-    'returnx+[+]+[1]'
-    >>> align_skeleton(skeleton="while x<y", code="for x<y")
-    '+[f]+[o]+[r]-[w]-[h]-[i]-[l]-[e]x<y'
-    >>> align_skeleton(skeleton="def f(x):", code="def g(x):")
-    'def+[g]-[f](x):'
-    """
-    skeleton, code = skeleton.replace(" ", ""), code.replace(" ", "")
-
-    def helper_align(skeleton_idx, code_idx):
-        """
-        Aligns the given skeletal segment with the code.
-        Returns (match, cost)
-            match: the sequence of corrections as a string
-            cost: the cost of the corrections, in edits
-        """
-        if skeleton_idx == len(skeleton) and code_idx == len(code):
-            return _________, ______________
-        if skeleton_idx < len(skeleton) and code_idx == len(code):
-            edits = "".join(["-[" + c + "]" for c in skeleton[skeleton_idx:]])
-            return _________, ______________
-        if skeleton_idx == len(skeleton) and code_idx < len(code):
-            edits = "".join(["+[" + c + "]" for c in code[code_idx:]])
-            return _________, ______________
-
-        possibilities = []
-        skel_char, code_char = skeleton[skeleton_idx], code[code_idx]
-        # Match
-        if skel_char == code_char:
-            _________________________________________
-            _________________________________________
-            possibilities.append((_______, ______))
-        # Insert
-        _________________________________________
-        _________________________________________
-        possibilities.append((_______, ______))
-        # Delete
-        _________________________________________
-        _________________________________________
-        possibilities.append((_______, ______))
-        return min(possibilities, key=lambda x: x[1])
-    result, cost = ________________________
-    return result
+# class Check(object):
+#     "*** YOUR CODE HERE ***"
 
 
-def foldl(link, fn, z):
-    """ Left fold
-    >>> lst = Link(3, Link(2, Link(1)))
-    >>> foldl(lst, sub, 0) # (((0 - 3) - 2) - 1)
-    -6
-    >>> foldl(lst, add, 0) # (((0 + 3) + 2) + 1)
-    6
-    >>> foldl(lst, mul, 1) # (((1 * 3) * 2) * 1)
-    6
-    """
-    if link is Link.empty:
-        return z
-    "*** YOUR CODE HERE ***"
-    return foldl(______, ______, ______)
+# def align_skeleton(skeleton, code):
+#     """
+#     Aligns the given skeleton with the given code, minimizing the edit distance between
+#     the two. Both skeleton and code are assumed to be valid one-line strings of code.
+
+#     >>> align_skeleton(skeleton="", code="")
+#     ''
+#     >>> align_skeleton(skeleton="", code="i")
+#     '+[i]'
+#     >>> align_skeleton(skeleton="i", code="")
+#     '-[i]'
+#     >>> align_skeleton(skeleton="i", code="i")
+#     'i'
+#     >>> align_skeleton(skeleton="i", code="j")
+#     '+[j]-[i]'
+#     >>> align_skeleton(skeleton="x=5", code="x=6")
+#     'x=+[6]-[5]'
+#     >>> align_skeleton(skeleton="return x", code="return x+1")
+#     'returnx+[+]+[1]'
+#     >>> align_skeleton(skeleton="while x<y", code="for x<y")
+#     '+[f]+[o]+[r]-[w]-[h]-[i]-[l]-[e]x<y'
+#     >>> align_skeleton(skeleton="def f(x):", code="def g(x):")
+#     'def+[g]-[f](x):'
+#     """
+#     skeleton, code = skeleton.replace(" ", ""), code.replace(" ", "")
+
+#     def helper_align(skeleton_idx, code_idx):
+#         """
+#         Aligns the given skeletal segment with the code.
+#         Returns (match, cost)
+#             match: the sequence of corrections as a string
+#             cost: the cost of the corrections, in edits
+#         """
+#         if skeleton_idx == len(skeleton) and code_idx == len(code):
+#             return _________, ______________
+#         if skeleton_idx < len(skeleton) and code_idx == len(code):
+#             edits = "".join(["-[" + c + "]" for c in skeleton[skeleton_idx:]])
+#             return _________, ______________
+#         if skeleton_idx == len(skeleton) and code_idx < len(code):
+#             edits = "".join(["+[" + c + "]" for c in code[code_idx:]])
+#             return _________, ______________
+
+#         possibilities = []
+#         skel_char, code_char = skeleton[skeleton_idx], code[code_idx]
+#         # Match
+#         if skel_char == code_char:
+#             _________________________________________
+#             _________________________________________
+#             possibilities.append((_______, ______))
+#         # Insert
+#         _________________________________________
+#         _________________________________________
+#         possibilities.append((_______, ______))
+#         # Delete
+#         _________________________________________
+#         _________________________________________
+#         possibilities.append((_______, ______))
+#         return min(possibilities, key=lambda x: x[1])
+#     result, cost = ________________________
+#     return result
 
 
-def filterl(lst, pred):
-    """ Filters LST based on PRED
-    >>> lst = Link(4, Link(3, Link(2, Link(1))))
-    >>> filterl(lst, lambda x: x % 2 == 0)
-    Link(4, Link(2))
-    """
-    "*** YOUR CODE HERE ***"
+# def foldl(link, fn, z):
+#     """ Left fold
+#     >>> lst = Link(3, Link(2, Link(1)))
+#     >>> foldl(lst, sub, 0) # (((0 - 3) - 2) - 1)
+#     -6
+#     >>> foldl(lst, add, 0) # (((0 + 3) + 2) + 1)
+#     6
+#     >>> foldl(lst, mul, 1) # (((1 * 3) * 2) * 1)
+#     6
+#     """
+#     if link is Link.empty:
+#         return z
+#     "*** YOUR CODE HERE ***"
+#     return foldl(______, ______, ______)
 
 
-def reverse(lst):
-    """ Reverses LST with foldl
-    >>> reverse(Link(3, Link(2, Link(1))))
-    Link(1, Link(2, Link(3)))
-    >>> reverse(Link(1))
-    Link(1)
-    >>> reversed = reverse(Link.empty)
-    >>> reversed is Link.empty
-    True
-    """
-    "*** YOUR CODE HERE ***"
+# def filterl(lst, pred):
+#     """ Filters LST based on PRED
+#     >>> lst = Link(4, Link(3, Link(2, Link(1))))
+#     >>> filterl(lst, lambda x: x % 2 == 0)
+#     Link(4, Link(2))
+#     """
+#     "*** YOUR CODE HERE ***"
 
 
-def identity(x): return x
+# def reverse(lst):
+#     """ Reverses LST with foldl
+#     >>> reverse(Link(3, Link(2, Link(1))))
+#     Link(1, Link(2, Link(3)))
+#     >>> reverse(Link(1))
+#     Link(1)
+#     >>> reversed = reverse(Link.empty)
+#     >>> reversed is Link.empty
+#     True
+#     """
+#     "*** YOUR CODE HERE ***"
 
 
-def foldl2(link, fn, z):
-    """ Write foldl using foldr
-    >>> list = Link(3, Link(2, Link(1)))
-    >>> foldl2(list, sub, 0) # (((0 - 3) - 2) - 1)
-    -6
-    >>> foldl2(list, add, 0) # (((0 + 3) + 2) + 1)
-    6
-    >>> foldl2(list, mul, 1) # (((1 * 3) * 2) * 1)
-    6
-    """
-    def step(x, g):
-        "*** YOUR CODE HERE ***"
-    return foldr(link, step, identity)(z)
+# def identity(x): return x
+
+
+# def foldl2(link, fn, z):
+#     """ Write foldl using foldr
+#     >>> list = Link(3, Link(2, Link(1)))
+#     >>> foldl2(list, sub, 0) # (((0 - 3) - 2) - 1)
+#     -6
+#     >>> foldl2(list, add, 0) # (((0 + 3) + 2) + 1)
+#     6
+#     >>> foldl2(list, mul, 1) # (((1 * 3) * 2) * 1)
+#     6
+#     """
+#     def step(x, g):
+#         "*** YOUR CODE HERE ***"
+#     return foldr(link, step, identity)(z)
 
 
 class Tree:
--- lab/lab14/lab14.scm
+++ lab/lab14/lab14.scm
@@ -1,9 +1,20 @@
 (define (split-at lst n)
-  'YOUR-CODE-HERE
+  (define (helper new old n)
+    (cond ((eq? old nil) (cons new nil))
+          ((= n 0) (cons new old))
+          (else (helper (append new (car old)) (cdr old) (- n 1)))))
+  (helper nil lst n)
 )
 
 
 (define (compose-all funcs)
-  'YOUR-CODE-HERE
+  (lambda (x)
+    (if (eq? funcs nil)
+        x
+        ((compose-all (cdr funcs)) ((car funcs) x))))
 )
 
+(define (append lst item)
+      (if (eq? lst nil)
+          (cons item nil)
+          (cons (car lst) (append (cdr lst) item))))
